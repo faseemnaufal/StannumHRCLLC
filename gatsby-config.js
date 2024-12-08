098481fa96b26c945b5ab8aafec54859f6d25790
faseemnaufal <faseemnaufal@gmail.com>
2024-12-08 19:05:41 +0530

    new files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,8 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
-const adapter = require("gatsby-adapter-netlify");
 
 module.exports = {
-  adapter: adapter({
-    excludeDatastoreFromEngineFunction: false
-  }),
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
